987d6706dcf32857424516e74f7a7e99d5fac9e2
Tawhid Shah <tawhidshahdev@gmail.com>
2025-08-26 02:26:18 +0100

    Inlined constants as they're only used in one place

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,30 @@
 import Link from "next/link";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, ShieldCheck, Smartphone, RefreshCw } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
-import homeCardContent from "@/constants/homeCardContent";
+const homeCardContent = [
+  {
+    icon: ShieldCheck,
+    title: "Secure Storage",
+    content:
+      "Protect what matters most. TeraStore provides ironclad security for your files, employing robust encryption protocols to keep your data safe and confidential.",
+  },
+  {
+    icon: Smartphone,
+    title: "Cross-Platform Compatibility",
+    content:
+      "Access anytime, anywhere. TeraStore seamlessly integrates with all your devices and platforms, allowing you to access your files from anywhere, regardless of the device you're using.",
+  },
+  {
+    icon: RefreshCw,
+    title: "Real-Time Sync",
+    content:
+      "Stay updated, everywhere. TeraStore keeps your files synchronized in real-time across all your devices, ensuring you have the latest version at your fingertips.",
+  },
+];
 
 export default function Home() {
   return (
